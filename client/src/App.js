d9197ae7e7038b79cf6e5af81855c610acc0e3a7
saipavan1239 <saipavan12399@gmail.com>
2024-06-23 10:45:00 +0530

    frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
-import Router from './Components/Router'
+import logo from "./logo.svg";
+import "./App.css";
+import Sponser from "./Component/Sponser";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./Component/About";
+import Attendence from "./Component/Attendence";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
-    <div>
-      <Router/>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Sponser />} />
+          <Route path="/about" element={<About />} />
+          {/* <Route path="/attendence" element={<Attendence />} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
