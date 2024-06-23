a38d6cf4e21d052f89373700ed1fff6faaa06622
Sree Vaishnavi <sreevaishnavibharathdwaja123@gmail.com>
2024-06-23 08:51:45 +0530

    footer added

--- client/src/Components/Router.js
+++ client/src/Components/Router.js
@@ -9,7 +9,7 @@ import Home from './Home/Home'
 import About from './About/About'
 import Profile from './Profile/Profile'
 import Page from './maps/Page'
-import Dashboard from './Dashboard/Dashboard'      
+     
 
 function Router() {
     let {user}=useContext(CounterContext)
@@ -53,3 +53,4 @@ function Router() {
   )
 }
 
+export default Router;
